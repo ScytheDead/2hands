e33c18e996f705e6edc66065269aaa81e1a0b6d2
Huy Nguyễn <scythedead.h@gmail.com>
2019-05-08 15:06:37 +0700

    finish feature users

---
+++ api/controllers/users.js
@@ -0,0 +1,116 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/users');
+
+exports.user_signup = (req, res, next) => {
+    // console.log(req.body);
+    User.find({
+        phoneNumber: req.body.phoneNumber
+        })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({ //409: conflict, 422: unprocess about entity
+                    message: 'Phone number exists'
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            phoneNumber: req.body.phoneNumber,
+                            password: hash
+                        });
+                        user
+                            .save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: 'User created'
+                                });
+                            })
+                            .catch(err => {
+                                res.status(500).json({
+                                    error: err
+                                });
+                            })
+                    }
+                });
+            }
+        })
+}
+
+exports.user_login = (req, res, next) => {
+    User.findOne({
+        phoneNumber: req.body.phoneNumber
+        })
+        .exec()
+        .then(user => {
+            
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+                if (result) {
+                    const token = jwt.sign( //parse dữ liệu thành chuỗi token
+                        {
+                            phoneNumber: user.phoneNumber,
+                            userId: user._id
+                        },
+                        process.env.JWT_KEY, {
+                            expiresIn: '1h'
+                        }
+                    );
+                    return res.status(200).json({
+                        message: 'Auth successful',
+                        token: token //gửi chuỗi token về client
+                    });
+                }
+                res.status(401).json({
+                    message: 'Auth failed'
+                });
+            });
+        })
+        .catch(err => {
+            console.log(3333)
+            res.status(500).json({
+                error: err
+            });
+        });
+}
+
+exports.user_delete = (req, res, next) => {
+    User.deleteOne({
+            _id: req.params.userId
+        })
+        .exec()
+        .then(result => {
+            console.log(result.deletedCount);
+            if(result.deletedCount == 0) {
+                return res.status(500).json({
+                    error: "Not found user"
+                });
+            }
+            res.status(200).json({
+                message: 'User deleted'
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+}
--- api/models/users.js
+++ api/models/users.js
@@ -3,7 +3,7 @@ mongoose.set('useCreateIndex', true);
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    phoneNumber: { type: String, required: true, unique: true, match: /(09|01[2|6|8|9])+([0-9]{8})\b/ },
+    phoneNumber: { type: Number, required: true, unique: true, match: /(09|01[2|6|8|9])+([0-9]{8})\b/ },
     password: { type: String, required: true },
     name: {type: String, trim: true},
     address: {type: String, trim: true },
@@ -11,6 +11,7 @@ const userSchema = mongoose.Schema({
     facebook: {type: String, trim: true },
     email: {type: String, match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ },
     gender: { type: Boolean },
+    rule: { type: Number, default: 0},
     subscribes: [{ type: String }],
     messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message', required: true }],
     notes: {type: String }
