0fa08af93880100c420b0b867f9016a25206375a
Huy Nguyễn <scythedead.h@gmail.com>
2019-05-24 17:57:13 +0700

    complete module producer backend

--- api/models/producers.js
+++ api/models/producers.js
@@ -4,7 +4,7 @@ mongoose.set('useCreateIndex', true);
 const producerSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     classify: { type: mongoose.Schema.Types.ObjectId, ref: 'Classify', required: true },
-    title: { type: String, required:true, trim: true, min: 1, max: 50 },
+    title: { type: String, required: true, trim: true, min: 1, max: 50 },
     image: { type: String, required: true},
     note: { type: String }
 },{ timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
