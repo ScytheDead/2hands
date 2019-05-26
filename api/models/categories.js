1bb0edeac17627c1f9145f8176f6536bdbaa97d6
Huy Nguyễn <scythedead.h@gmail.com>
2019-05-26 21:46:21 +0700

    change receive file image to base64

--- api/models/categories.js
+++ api/models/categories.js
@@ -3,7 +3,7 @@ mongoose.set('useCreateIndex', true);
 
 const categorySchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    title: { type: String, required: true, unique: true, trim: true, min: 5, max: 100 },
+    title: { type: String, required: true, unique: true, trim: true, minlength: [5, 'title too short'], maxlength: [100, 'title too long'] },
     image: { type: String, required: true},
     note: { type: String }
 },{ timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
