cf73a7183de039f4837aef57d1df1036582a427b
Huy Nguyễn <scythedead.h@gmail.com>
2019-06-11 02:23:42 +0700

    fix loaded

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,3 +1,13 @@
-setTimeout(() => {
-    document.getElementsByTagName("body")[0].setAttribute("class", "loaded");
-}, 1200);
+loadPage();
+
+function loadPageAnimation(){
+    return new Promise(resolve => {
+        setTimeout(() => {
+            document.getElementsByTagName("body")[0].setAttribute("class", "loaded");
+        }, 1200);
+    })
+}
+
+async function loadPage(){
+    await loadPageAnimation();
+}
