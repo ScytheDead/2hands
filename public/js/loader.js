6c953c1ec65b00ac730a22c4eb0348c64df72727
Huy Nguyễn <scythedead.h@gmail.com>
2019-06-11 02:38:32 +0700

    fix loaded

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,21 +1,4 @@
 setTimeout(() => {
-    // document.getElementsByTagName("body")[0].setAttribute("class", "loaded");
-    bodyId.classList.add('loaded');
+    document.getElementsByTagName("body")[0].setAttribute("class", "loaded");
 }, 1200);
-
-
-// loadPage();
-
-// function loadPageAnimation(){
-//     return new Promise(resolve => {
-//         setTimeout(() => {
-//             // document.getElementsByTagName("body")[0].setAttribute("class", "loaded");
-//             bodyId.classList.add('loaded');
-//         }, 1200);
-//     })
-// }
-
-// async function loadPage(){
-//     await loadPageAnimation();
-// }
 
