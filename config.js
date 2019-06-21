d59f3c1afc4fc620198e23f9cae9318cef74466f
Huy Nguyễn <scythedead.h@gmail.com>
2019-06-22 06:50:00 +0700

    fix bug backend categories

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.API_ADDRESS = 'http://localhost:3000';
-// exports.API_ADDRESS = 'https://secondhandsapp.herokuapp.com';
+// exports.API_ADDRESS = 'http://localhost:3000';
+exports.API_ADDRESS = 'https://secondhandsapp.herokuapp.com';
 exports.JWT_KEY = 'secret';
 exports.MONGO_ATLAS_PW = '2hands-huy';
 exports.TIME_EXPIRES = '4h'
